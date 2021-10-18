09054b88c4d44ae2b00ab3e021e588dcfa22ff03
Nauftams <naufaltams@gmail.com>
2021-10-18 20:03:16 +0700

    add some style and sidebar

---
+++ src/component/sidebar/SidebarComp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SidebarComp = () => {
+  return (
+    <div className="pa-3">
+      <h2 className="text-center mt-3">Welcome to Manageify</h2>
+      <p className="text-center">Manageify is a simple webApp for help you to manage your job and time better than before.Thank you for using us</p>
+    </div>
+  );
+};
+
+export default SidebarComp;
